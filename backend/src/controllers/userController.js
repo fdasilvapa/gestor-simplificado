99145a4fd4c7844ccc506f2db0fd16751cde80f9
Felipe <felipedasilva23785@gmail.com>
2025-10-08 19:39:37 -0300

    feat(user): Implementa lógica de criação e login de usuários

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,21 +1,33 @@
-export const createUser = (req, res) => {
-    // Lógica para criar usuário virá aqui...
-    res.status(201).json({ message: "Usuário criado com sucesso" });
+import * as userService from '../services/userService.js';
+
+export const createUser = async (req, res) => {
+    try {
+        const newUser = await userService.register(req.body);
+        res.status(201).json({ message: "Usuário criado com sucesso" }, newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 };
 
-export const getAllUsers = (req, res) => {
-    // Lógica para listar usuários virá aqui...
-    res.status(200).json({ message: "Usuários listados" });
-};
+export const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const result = await userService.login(email, password);
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(401).json({ message: error.message });
+    }
+}
 
-export const updateUser = (req, res) => {
+export const updateUser = async (req, res) => {
     const { id } = req.params;
     // Lógica para atualizar usuário virá aqui...
     res.status(200).json({ message: `Usuário com ID ${id} atualizado com sucesso` });
 };
 
-export const deleteUser = (req, res) => {
-    const { id } = req.params;
-    // Lógica para deletar usuário virá aqui...
-    res.status(200).json({ message: `Usuário com ID ${id} deletado com sucesso` });
-};
+// Funcionalidade futura
+// export const deleteUser = async (req, res) => {
+//     const { id } = req.params;
+//     // Lógica para deletar usuário virá aqui...
+//     res.status(200).json({ message: `Usuário com ID ${id} deletado com sucesso` });
+// };
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,16 +1,19 @@
 import { Router } from "express";
 import {
   createUser,
-  getAllUsers,
+  loginUser,
   updateUser,
-  deleteUser,
+  //deleteUser,
 } from "../controllers/userController.js";
 
 const router = Router();
 
-router.post("/", createUser); // POST /api/users/
-router.get("/", getAllUsers); // GET /api/users/
+router.post("/register", createUser); // POST /api/users/register
+router.post("/login", loginUser); // POST /api/users/login
+
 router.put("/:id", updateUser); // PUT /api/users/123
-router.delete("/:id", deleteUser); // DELETE /api/users/123
+
+// Funcionalidade futura
+// router.delete("/:id", deleteUser); // DELETE /api/users/123
 
 export default router;
