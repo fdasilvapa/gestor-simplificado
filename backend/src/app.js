48a5f613321ac38fae12f4181ffefea9be1971b6
Felipe <felipedasilva23785@gmail.com>
2025-10-11 12:38:14 -0300

    feat(product): Implementa rotas do product e as integra ao app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // Define um prefixo para todas as rotas de API e usa o roteador de usuários
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 
 export default app;
