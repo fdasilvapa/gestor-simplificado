9a2c4a778c1428c7cfad51aab93450f7bfa6fdb9
Felipe <felipedasilva23785@gmail.com>
2025-10-21 11:19:07 -0300

    feat(context): Implementa um authContext para gerenciar a autenticação

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,8 +15,6 @@ function Login() {
 
   return (
     <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-3xl font-bold text-center text-green-600">Login</h2>
-      <p className="text-center text-gray-600">Bem vindo de volta!</p>
       <form onSubmit={handleSubmit} className="space-y-6">
         {/* Campo de e-mail */}
         <div>
