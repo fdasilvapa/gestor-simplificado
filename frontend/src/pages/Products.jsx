1dee1460242823aa89dae0a22b9aadf97df94dca
Felipe <felipedasilva23785@gmail.com>
2025-10-30 10:05:18 -0300

    feat(produtos): Implementa modal e formulario de criação de produtos

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { getProducts } from "../services/productService";
+import { getProducts, createProduct } from "../services/productService";
 import { Loader2, Plus, Edit, Trash2 } from "lucide-react";
+import Modal from "../components/ui/Modal";
+import ProductForm from "../components/ProductForm";
 
 const formatCurrency = (value) => {
   return parseFloat(value).toLocaleString("pt-BR", {
@@ -13,6 +15,10 @@ function Products() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formError, setFormError] = useState(null);
 
   const fetchProducts = async () => {
     try {
@@ -31,9 +37,30 @@ function Products() {
     fetchProducts();
   }, []);
 
-  const handleCreate = () => {
-    // TODO: Abrir modal de criação
-    alert('Função "Novo produto" a ser implementada!');
+  const handleCreateClick = () => {
+    setFormError(null);
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    if (isSubmitting) return;
+    setIsModalOpen(false);
+  };
+
+  const handleSaveProduct = async (productData) => {
+    setFormError(null);
+    setIsSubmitting(true);
+
+    try {
+      await createProduct(productData);
+
+      setIsModalOpen(false);
+      fetchProducts();
+    } catch (err) {
+      setFormError(err.message || "Falha ao salvar produto.");
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   const handleEdit = (id) => {
@@ -134,6 +161,20 @@ function Products() {
           </tbody>
         </table>
       </div>
+
+      {/* Modal */}
+      <Modal
+        title="Novo Produto"
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+      >
+        <ProductForm
+          onSave={handleSaveProduct}
+          onCancel={handleCloseModal}
+          apiError={formError}
+          isSubmitting={isSubmitting}
+        />
+      </Modal>
     </div>
   );
 }
